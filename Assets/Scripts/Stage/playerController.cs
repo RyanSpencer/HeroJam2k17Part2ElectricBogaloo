6a86d6cac9b10c780338095604a9fd74ec5de04e
Ryan Spencer (RIT Student) <rxs2833@ad.rit.edu>
2017-10-27 23:40:11 -0400

    Added initial work

---
+++ Assets/Scripts/Stage/playerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour {
+
+    public GameObject manager;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.W))
+        { 
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+
+        }
+	}
+}
