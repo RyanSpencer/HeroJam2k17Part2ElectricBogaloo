2b972a29c1644b9d2e26102f837faa18f40e92f6
Raymond Stempel (RIT Student) <rs6571@ad.rit.edu>
2017-10-27 22:58:53 -0400

    COMMITING EARLY FILES
    Commiting early scripts and framewor

---
+++ Assets/Puzzle_Scripts/Puzzle.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Puzzle : MonoBehaviour {
+
+    public enum puzzleObj {
+
+        rock,
+        plate,
+        matche,
+        bush,
+        bucket,
+        fire,
+        wire,
+        generator,
+        powerNode,
+        door
+
+    }
+
+    public puzzleObj objType;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    /*public bool interactDoor() {
+
+        bool doorBool;
+
+        return doorBool;
+
+    }
+
+    public bool interactBush() {
+
+        bool bushBool;
+
+        return bushBool;
+
+    }
+
+    public bool interactPlate() {
+
+        bool plateBool;
+
+        return plateBool;
+
+    }
+
+    public bool interactWire() {
+
+        bool wireBool;
+
+        return wireBool;
+
+    }*/
+
+}
---
+++ Assets/Puzzle_Scripts/Puzzle_Manager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Puzzle_Manager : MonoBehaviour {
+
+    private enum puzzleType {
+
+        Plate,
+        Rock,
+        Wire,
+        Bush,
+        Door
+
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void puzzleInteract(Puzzle objOne, Puzzle objTwo) {
+
+
+
+    }
+}
